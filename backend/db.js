99a8a3c73e7091987de684699392ba879045cd25
henriquecmelo1 <hcm@cin.ufpe.br>
2025-07-04 11:26:34 -0300

    fix: database url

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ import postgres from "postgres";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const sql = postgres(`postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@postgres:5432/${process.env.POSTGRES_DB}`)
+export const sql = postgres(process.env.DATABASE_URL || `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@postgres:5432/${process.env.POSTGRES_DB}`);
 
 // Usuários
 
